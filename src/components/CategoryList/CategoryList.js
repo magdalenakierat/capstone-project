65165627f8ea7e5f3de3297b32ad1a7c486311a7
Magdalena Kierat <m.kierat@gmx.de>
2022-07-27 14:42:37 +0200

    fix styling

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -33,6 +33,7 @@ const StyledButton = styled.button`
   border-radius: 1.25rem;
   border: none;
   background: #e6e8e9;
+  color: #000;
   font-size: 0.875rem;
   font-weight: 500;
   line-height: 1.25rem;
