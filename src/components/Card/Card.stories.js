750cd29d7e15883296b9b04ef8eb85128376d487
Magdalena Kierat <m.kierat@gmx.de>
2022-08-12 15:39:33 +0200

    update card stories

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -24,5 +24,6 @@ export const Default = () => (
     title={exampleExercise.title}
     description={exampleExercise.description}
     duration={exampleExercise.duration}
+    onBookmark={() => console.log('Bookmark was clicked.')}
   />
 );
